02147e25d4be0801e2367673197a5beb271f7ad5
Lucas Costa <lucasvinicius3105@gmail.com>
2023-04-26 23:29:40 -0300

    :init: iniciando o projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { BannerMainComponent } from './banner-main/banner-main.component';
+import { GaleryComponent } from './galery/galery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    BannerMainComponent,
+    GaleryComponent
   ],
   imports: [
     BrowserModule,
