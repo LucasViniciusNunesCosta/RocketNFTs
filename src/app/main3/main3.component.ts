1b819ba38a9e88c64cd03e24ea2c480fa84cf888
Lucas Costa <lucasvinicius3105@gmail.com>
2023-05-01 23:15:04 -0300

    :feat: main3 ready

---
+++ src/app/main3/main3.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'main3',
+  templateUrl: './main3.component.html',
+  styleUrls: ['./main3.component.css']
+})
+export class Main3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
